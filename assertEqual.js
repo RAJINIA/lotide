83ad18c3574de75dfc4a349eb335481acad2b18f
RAJINIA <rajini1608@gmail.com>
2021-10-25 22:54:30 -0600

    emoji code

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(String.fromCodePoint(0x2705) + String.fromCodePoint(0x2705) +  "Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log(String.fromCodePoint(0x1F534) + String.fromCodePoint(0x1F534) + "Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual("bootcamp", "Bootcamp");
+assertEqual(1, 100);
+
+
+//U+1F534
