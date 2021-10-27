5a4de6b6ec7a957c64bc919fef64605054af457f
RAJINIA <rajini1608@gmail.com>
2021-10-27 14:03:37 -0600

    adding assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`String.fromCodePoint(0x2705) String.fromCodePoint(0x2705) Assertion Passed: ${actual} === ${expected}`);
-    //console.log(String.fromCodePoint(0x2705) + String.fromCodePoint(0x2705) +  "Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅✅Assertion Passed: ${actual} = ${expected}`);
   } else {
-    console.log(`String.fromCodePoint(0x1F534)  String.fromCodePoint(0x1F534) Assertion Failed: ${actual} !== ${expected}`);
-    //console.log(String.fromCodePoint(0x1F534) + String.fromCodePoint(0x1F534) + "Assertion Failed: " + actual + " !== " + expected);
-    : 
+    console.log(`❌❌Assertion Failed: ${actual} != ${expected}`);
   }
 };
 
