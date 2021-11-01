c36a76f28b8148fddaf6f7c5ed8f3b5578c91702
RAJINIA <rajini1608@gmail.com>
2021-11-01 16:21:48 -0600

    refactor assertArrayEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -12,7 +12,7 @@ const eqArrays = function(arr1, arr2){
   }
 }
 
-const assertArrayEqual = function(arr1, arr2) {
+const assertArraysEqual = function(arr1, arr2) {
   if(eqArrays(arr1, arr2) === true){
     console.log(`✅✅Assertion Passed: ${JSON.stringify(arr1)} = ${JSON.stringify(arr2)}`);
   } else {
@@ -20,13 +20,15 @@ const assertArrayEqual = function(arr1, arr2) {
   }
 }
 
-eqArrays([1, 2, 3], [1, 2, 3]);
-eqArrays([1, 2, 3], [3, 2, 1]);
-eqArrays(["1", "2", "3"], ["1", "2", "3"]);
-eqArrays(["1", "2", "3"], ["1", "2", 3]);
+// //TEST CODE
+// eqArrays([1, 2, 3], [1, 2, 3]);
+// eqArrays([1, 2, 3], [3, 2, 1]);
+// eqArrays(["1", "2", "3"], ["1", "2", "3"]);
+// eqArrays(["1", "2", "3"], ["1", "2", 3]);
 
-assertArrayEqual([1, 2, 3], [1, 2, 3]);
-assertArrayEqual([1, 2, 3], [3, 2, 1]);
-assertArrayEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArrayEqual(["1", "2", "3"], ["1", "2", 3]);
+// assertArraysEqual([1, 2, 3], [1, 2, 3]);
+// assertArraysEqual([1, 2, 3], [3, 2, 1]);
+// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
 
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,9 @@
+const assertArraysEqual = require('../assertArraysEqual');
+const eqArrays = require('../eqArrays');
+
+//TEST CODE
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
